a0007ef5107d7d027300aa87e5a47150e77bf3ef
Amit Dimri <dimriamit98@gmail.com>
2020-10-25 23:05:20 +0530

    prev site

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4860f1a8d9fb908c2f1a2578329540b0.js"
+  "/precache-manifest.77b7f61273bf00ad30ec218353acf005.js"
 );
 
 self.addEventListener('message', (event) => {
